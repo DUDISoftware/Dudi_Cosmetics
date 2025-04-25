33445688bed65b393e51dd02bc9b97953ed82730
trinhtuandan03 <trinhtuandan1601@gmail.com>
2025-04-25 08:43:09 +0700

    giao diện admin

--- frontend/frontend_shop/src/App.jsx
+++ frontend/frontend_shop/src/App.jsx
@@ -3,24 +3,38 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Form from "./pages/modules/Authorization/Form";
-import AdminPage from "./pages/Admin/AdminPage";
 import ProtectedRoute from "./components/ProtectedRoute"; // Import ProtectedRoute
 import Profile from "./pages/Profile/Profile";
+// Import các trang admin
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { baselightTheme } from "./theme/DefaultColors";
+import FullLayout from './pages/Admin/layouts/FullLayout';
+import Dashboard from "./pages/Admin/dashboard/Dashboard";
 
 const App = () => {
+  const theme = baselightTheme;
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/login" element={<Form isSignInPage={true} />} />
-        <Route path="/register" element={<Form isSignInPage={false} />} />
-        <Route path="/profile" element={<Profile/>}/>
-        <Route element={<ProtectedRoute requiredRole="admin" />}>
-          <Route path="/admin" element={<AdminPage />} />
-        </Route>
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          {/* User Routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/login" element={<Form isSignInPage={true} />} />
+          <Route path="/register" element={<Form isSignInPage={false} />} />
+          <Route path="/profile" element={<Profile />} />
+
+          {/* Admin Routes */}
+          <Route element={<ProtectedRoute requiredRole="admin" />}>
+            <Route path="/admin" element={<FullLayout />}>
+              <Route index element={<Dashboard />} />
+            </Route>
+          </Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 };
 
---
+++ frontend/frontend_shop/src/pages/Admin/layouts/FullLayout.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import { styled, Container, Box, Typography, Link } from "@mui/material";
+import { Outlet } from "react-router-dom";
+
+import Header from "../../../../src/pages/Admin/layouts/header/Header";
+import Sidebar from "../../../../src/pages/Admin/layouts/sidebar/Sidebar"
+
+const MainWrapper = styled("div")(() => ({
+  display: "flex",
+  minHeight: "100vh",
+  width: "100%",
+  overflow: "hidden",
+}));
+
+const PageWrapper = styled("div")(() => ({
+  display: "flex",
+  flexGrow: 1,
+  flexDirection: "column",
+  zIndex: 1,
+  backgroundColor: "transparent",
+  maxWidth: "100%",
+}));
+
+const FullLayout = () => {
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
+  const [isMobileSidebarOpen, setMobileSidebarOpen] = useState(false);
+  // const lgUp = useMediaQuery((theme) => theme.breakpoints.up("lg"));
+
+  return (
+    <MainWrapper className="mainwrapper">
+      {/* ------------------------------------------- */}
+      {/* Sidebar */}
+      {/* ------------------------------------------- */}
+      <Sidebar
+        isSidebarOpen={isSidebarOpen}
+        isMobileSidebarOpen={isMobileSidebarOpen}
+        onSidebarClose={() => setMobileSidebarOpen(false)}
+      />
+      {/* ------------------------------------------- */}
+      {/* Main Wrapper */}
+      {/* ------------------------------------------- */}
+      <PageWrapper className="page-wrapper">
+        {/* ------------------------------------------- */}
+        {/* Header */}
+        {/* ------------------------------------------- */}
+        <Header
+          toggleSidebar={() => setSidebarOpen(!isSidebarOpen)}
+          toggleMobileSidebar={() => setMobileSidebarOpen(true)}
+        />
+        {/* ------------------------------------------- */}
+        {/* PageContent */}
+        {/* ------------------------------------------- */}
+        <Container
+          sx={{
+            paddingTop: "20px",
+            maxWidth: "1200px",
+          }}
+        >
+          {/* ------------------------------------------- */}
+          {/* Page Route */}
+          {/* ------------------------------------------- */}
+          <Box sx={{ minHeight: "calc(100vh - 170px)" }}>
+            <Outlet />
+          </Box>
+
+          {/* ------------------------------------------- */}
+          {/* End Page */}
+          {/* ------------------------------------------- */}
+        </Container>
+        <Box sx={{ pt: 6, pb: 3, display: 'flex', justifyContent: 'center' }}>
+          <Typography>
+            © 2025 All rights reserved by
+            <Link target="_blank" href="https://www.adminmart.com">
+              <span>
+                AdminMart.com
+              </span>
+            </Link>
+          </Typography>
+
+          <Typography>
+            .Distributed by
+            <Link target="_blank" href="https://themewagon.com">
+              <span>
+                ThemeWagon
+              </span>
+            </Link>
+          </Typography>
+        </Box>
+      </PageWrapper>
+    </MainWrapper>
+  );
+};
+
+export default FullLayout;
